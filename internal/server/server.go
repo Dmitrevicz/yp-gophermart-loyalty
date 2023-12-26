83996e4d2396310d5d753d572a2344fa7e2f4369
Dmitry Kovalev <dmitrevicz@gmail.com>
2023-12-27 02:31:48 +0300

    Add gzip middleware
    Also minor retry progression fixes.

--- internal/server/server.go
+++ internal/server/server.go
@@ -41,9 +41,13 @@ func (s *server) configureRouter() {
 	gin.SetMode(s.cfg.GinMode)
 	s.router = gin.New()
 
-	s.router.Use(gin.Logger())       // writes requests logs to stdout
-	s.router.Use(h.Mids.LogErrors()) // writes errors to stderr using zap logger
-	s.router.Use(h.Mids.Recovery())
+	// setup middlewares
+	s.router.Use(
+		gin.Logger(),       // writes requests logs to stdout
+		h.Mids.LogErrors(), // writes errors to stderr using zap logger
+		h.Mids.Recovery(),
+		h.Mids.Gzip(),
+	)
 
 	api := s.router.Group("/api")
 	{
